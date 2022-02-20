14f625565e5c71a7e43c99676fa6806f3478a103
Johannes Quast <johannesquast@outlook.de>
2022-02-20 14:58:54 +0100

    Add basic annotations for dependency injection module.

---
+++ 0-ase-reinforcement-learning-utils/src/main/java/de/jquast/utils/di/annotations/Inject.java
@@ -0,0 +1,12 @@
+package de.jquast.utils.di.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.CONSTRUCTOR)
+public @interface Inject {
+
+}
