2c906a299bec17c442c67af3258624ac90fbceb0
Johannes Quast <johannesquast@outlook.de>
2022-03-01 17:53:14 +0100

    Add injection capabilities to factory.

--- 0-ase-reinforcement-learning-utils/src/main/java/de/jquast/utils/cli/command/impl/InjectingCommandFactory.java
+++ 0-ase-reinforcement-learning-utils/src/main/java/de/jquast/utils/cli/command/impl/InjectingCommandFactory.java
@@ -2,14 +2,19 @@ package de.jquast.utils.cli.command.impl;
 
 import de.jquast.utils.cli.command.CommandFactory;
 import de.jquast.utils.cli.command.analyzer.AnalyzedCommand;
-import de.jquast.utils.cli.command.annotations.Command;
-import de.jquast.utils.cli.command.exception.CommandException;
+import de.jquast.utils.cli.command.analyzer.OptionField;
+import de.jquast.utils.cli.command.analyzer.ParameterField;
+import de.jquast.utils.cli.command.annotations.Option;
+import de.jquast.utils.cli.command.annotations.Parameter;
+import de.jquast.utils.cli.command.arguments.ArgumentParser;
+import de.jquast.utils.cli.command.arguments.Arguments;
 import de.jquast.utils.di.InjectionContext;
 import de.jquast.utils.di.annotations.Inject;
 import de.jquast.utils.exception.InjectionException;
+import de.jquast.utils.reflection.ReflectionUtils;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class InjectingCommandFactory implements CommandFactory {
 
@@ -28,6 +33,8 @@ public class InjectingCommandFactory implements CommandFactory {
         }
 
         AnalyzedCommand<?> current = cmdMetadata;
+        int firstNonMatchingIndex = 1;
+
         for (int i = 1; i < parts.length; ++i) {
             String part = parts[i];
 
@@ -35,45 +42,67 @@ public class InjectingCommandFactory implements CommandFactory {
                 if (subCommand.command().name().equalsIgnoreCase(part)) {
                     current = subCommand;
                 } else {
+                    firstNonMatchingIndex = i;
                     break;
                 }
             }
         }
 
+        Arguments parsedArguments = ArgumentParser.parseArguments(parts, firstNonMatchingIndex);
 
+        try {
+            Object newInstance = context.createNewInstance(current.analyzedType().clazz());
+
+            try {
+                injectArguments(newInstance, parsedArguments, current);
+                injectParameters(newInstance, parsedArguments, current);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+
+            return newInstance;
+        } catch (InjectionException e) {
+            e.printStackTrace();
+        }
 
         return null;
     }
 
-    private Object createCommandInstanceRecursively(String[] parts, int currentIndex, AnalyzedCommand<?> current, Object parent) throws InjectionException {
-        if (currentIndex >= parts.length || !parts[currentIndex].equalsIgnoreCase(current.command().name())) {
-            return parent;
+    private void injectArguments(Object obj, Arguments arguments, AnalyzedCommand<?> commandMetadata) throws IllegalAccessException {
+        Map<String, OptionField> mappedOptions = new HashMap<>();
+        commandMetadata.options().forEach(option -> {
+            for (String name : option.optionAnnotation().names())
+                mappedOptions.put(name, option);
+        });
+
+        for (OptionField optionField : commandMetadata.options()) {
+            Option annotation = optionField.optionAnnotation();
+            boolean found = false;
+
+            // Suche Option
+            for (String name : annotation.names()) {
+                if (arguments.options().containsKey(name)) {
+                    if (found)
+                        throw new RuntimeException("Duplicated Option!");
+
+                    ReflectionUtils.setField(optionField.field(), obj, arguments.options().get(name));
+                    found = true;
+                }
+            }
+
+            if (!found && annotation.required())
+                throw new RuntimeException("Options " + String.join(", ", annotation.names()) + " not found.");
         }
+    }
 
-        Constructor target = current.analyzedType().injectionTarget();
-        Object[] dependencies = new Object[target.getParameterCount()];
-        int startIndex = 0;
+    private void injectParameters(Object obj, Arguments arguments, AnalyzedCommand<?> commandMetadata) throws IllegalAccessException {
+        for (ParameterField parameterField : commandMetadata.parameters()) {
+            Parameter parameter = parameterField.parameterAnnotation();
 
-        if (parent != null) {
-            dependencies[0] = parent;
-            startIndex = 1;
+            if (parameter.index() >= arguments.parameters().size())
+                throw new RuntimeException("Not enough parameters!");
+
+            ReflectionUtils.setField(parameterField.field(), obj, arguments.parameters().get(parameter.index()));
         }
-
-        for (int i = startIndex; i < dependencies.length; ++i) {
-            dependencies[i] = context.createNewInstance(target.getParameterTypes()[i]);
-        }
-
-        try {
-            Object obj = target.newInstance(dependencies);
-            return createCommandInstanceRecursively(parts, currentIndex + 1, current, obj);
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        }
-
-        return null;
     }
 }
