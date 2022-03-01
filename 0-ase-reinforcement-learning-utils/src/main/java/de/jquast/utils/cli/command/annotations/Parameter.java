937b35ebfc7967a066815de24c43984fa2066ccc
Johannes Quast <johannesquast@outlook.de>
2022-03-01 17:51:40 +0100

    Add missing index to Parameter annotation.

--- 0-ase-reinforcement-learning-utils/src/main/java/de/jquast/utils/cli/command/annotations/Parameter.java
+++ 0-ase-reinforcement-learning-utils/src/main/java/de/jquast/utils/cli/command/annotations/Parameter.java
@@ -1,4 +1,12 @@
 package de.jquast.utils.cli.command.annotations;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.METHOD})
 public @interface Parameter {
+    int index();
 }
