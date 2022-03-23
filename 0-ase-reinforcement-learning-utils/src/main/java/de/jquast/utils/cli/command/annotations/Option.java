f57607c983940687bef44f4a277ff963483ae004
Johannes Quast <johannesquast@outlook.de>
2022-03-23 13:23:18 +0100

    Add required description String to Option annotation.

--- 0-ase-reinforcement-learning-utils/src/main/java/de/jquast/utils/cli/command/annotations/Option.java
+++ 0-ase-reinforcement-learning-utils/src/main/java/de/jquast/utils/cli/command/annotations/Option.java
@@ -10,6 +10,8 @@ import java.lang.annotation.Target;
 public @interface Option {
     String[] names();
 
+    String description();
+
     boolean required() default false;
 
     String defaultValue() default "";
