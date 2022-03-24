9c551a700ec3861de259a30ff2c0158167a0f4e5
Johannes Quast <johannesquast@outlook.de>
2022-03-24 09:31:47 +0100

    Add additional error messages and add simple help command.

--- 0-ase-reinforcement-learning-utils/src/main/java/de/jquast/utils/cli/command/impl/StatelessCommandExecutionEngine.java
+++ 0-ase-reinforcement-learning-utils/src/main/java/de/jquast/utils/cli/command/impl/StatelessCommandExecutionEngine.java
@@ -4,13 +4,21 @@ import de.jquast.utils.cli.command.CommandExecutionEngine;
 import de.jquast.utils.cli.command.CommandFactory;
 import de.jquast.utils.cli.command.analyzer.AnalyzedCommand;
 import de.jquast.utils.cli.command.analyzer.CommandAnalyzer;
+import de.jquast.utils.cli.command.analyzer.OptionField;
+import de.jquast.utils.cli.command.analyzer.ParameterField;
+import de.jquast.utils.cli.command.annotations.Command;
+import de.jquast.utils.cli.command.annotations.Option;
+import de.jquast.utils.cli.command.annotations.Parameter;
 import de.jquast.utils.cli.command.exception.CommandAnalyzerException;
+import de.jquast.utils.cli.command.exception.CommandCreationException;
 import de.jquast.utils.cli.command.exception.CommandException;
 import de.jquast.utils.di.annotations.Inject;
+import de.jquast.utils.di.annotations.Singleton;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Singleton
 public class StatelessCommandExecutionEngine implements CommandExecutionEngine {
 
     private final CommandFactory commandFactory;
@@ -19,6 +27,8 @@ public class StatelessCommandExecutionEngine implements CommandExecutionEngine {
     @Inject
     public StatelessCommandExecutionEngine(CommandFactory commandFactory) {
         this.commandFactory = commandFactory;
+
+        registerCommand(DefaultHelpCommand.class);
     }
 
     @Override
@@ -30,12 +40,18 @@ public class StatelessCommandExecutionEngine implements CommandExecutionEngine {
 
         String topLevelCommand = parts[0];
         if (!commandRegistry.containsKey(topLevelCommand)) {
-            throw new CommandException(topLevelCommand, "Der angegebene Befehl wurde nicht gefunden.");
+            System.out.println(generateCommandList());
+            throw new CommandException(topLevelCommand, String.format("Der angegebene Befehl wurde nicht gefunden. \n%s", generateCommandList()));
         }
 
         // Command Object erstellen
         AnalyzedCommand<?> analyzedTopLevelCommand = commandRegistry.get(topLevelCommand);
-        Object commandInstance = commandFactory.createCommandInstance(command, analyzedTopLevelCommand);
+        Object commandInstance = null;
+        try {
+            commandInstance = commandFactory.createCommandInstance(command, analyzedTopLevelCommand);
+        } catch (CommandCreationException e) {
+            throw new CommandException(command, String.format("%s", generateUsage(e.getAffectedCommand())));
+        }
 
         if (!Runnable.class.isAssignableFrom(commandInstance.getClass())) {
             throw new CommandException(command, "Das Objekt des Commands ist nicht ausführbar, bitte Runnable implementieren.");
@@ -54,4 +70,49 @@ public class StatelessCommandExecutionEngine implements CommandExecutionEngine {
             e.printStackTrace();
         }
     }
+
+    private String generateCommandList() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Liste der zur Verfügung stehenden Befehle: \n");
+
+        for (AnalyzedCommand<?> cmd : commandRegistry.values()) {
+            builder.append(String.format("  %-15s%s\n", cmd.command().name(), cmd.command().description()));
+        }
+
+        return builder.toString();
+    }
+
+    private String generateUsage(AnalyzedCommand<?> cmd) {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("Korrekte Nutzung:\n");
+        builder.append("  Parameter:\n");
+        for (ParameterField paraField : cmd.parameters()) {
+            Parameter parameter = paraField.parameterAnnotation();
+
+            builder.append(String.format("    [#%d] %s\n", parameter.index(), parameter.description()));
+        }
+
+        builder.append("  Optionen:\n");
+        for (OptionField optionField : cmd.options()) {
+            Option option = optionField.optionAnnotation();
+
+            builder.append(String.format("    %-20s %s\n", String.join(", ", option.names()), option.description()));
+        }
+
+        return builder.toString();
+    }
+
+    @Command(
+            name = "help",
+            description = "Gibt hilfreiche Informationen aus."
+    )
+    public class DefaultHelpCommand implements Runnable {
+
+        @Override
+        public void run() {
+            System.out.println(generateCommandList());
+        }
+    }
+
 }
