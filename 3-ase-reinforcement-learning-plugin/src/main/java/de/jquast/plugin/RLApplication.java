3c56e12573be7a12fc348e958e0db2afc8abdc97
Johannes Quast <johannesquast@outlook.de>
2022-03-28 21:13:00 +0200

    Add basic main Application and configure simple mappings.

--- 3-ase-reinforcement-learning-plugin/src/main/java/de/jquast/plugin/RLApplication.java
+++ 3-ase-reinforcement-learning-plugin/src/main/java/de/jquast/plugin/RLApplication.java
@@ -2,6 +2,9 @@ package de.jquast.plugin;
 
 import de.jquast.utils.cli.command.CommandExecutionEngine;
 import de.jquast.utils.cli.command.CommandFactory;
+import de.jquast.utils.cli.command.converter.ArgumentConverters;
+import de.jquast.utils.cli.command.converter.impl.DefaultArgumentConverters;
+import de.jquast.utils.cli.command.exception.CommandException;
 import de.jquast.utils.cli.command.impl.InjectingCommandFactory;
 import de.jquast.utils.cli.command.impl.StatelessCommandExecutionEngine;
 import de.jquast.utils.di.InjectionContext;
@@ -16,12 +19,13 @@ public class RLApplication {
 
         CONTEXT.mapInterface(CommandExecutionEngine.class, StatelessCommandExecutionEngine.class);
         CONTEXT.mapInterface(CommandFactory.class, InjectingCommandFactory.class);
+        CONTEXT.mapInterface(ArgumentConverters.class, DefaultArgumentConverters.class);
     }
 
-    public static void main(String[] args) throws InjectionException {
+    public static void main(String[] args) throws InjectionException, CommandException {
         CommandExecutionEngine engine = CONTEXT.createNewInstance(CommandExecutionEngine.class);
 
-
+        engine.execute(String.join(" ", args));
     }
 
 }
