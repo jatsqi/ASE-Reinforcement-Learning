755aace8db15a0cd462fdb09a90308bd702fe284
Johannes Quast <johannesquast@outlook.de>
2022-03-30 11:13:50 +0200

    Add additional config options for algorithms.

--- 1-ase-reinforcement-learning-domain/src/main/java/de/jquast/domain/config/ConfigItems.java
+++ 1-ase-reinforcement-learning-domain/src/main/java/de/jquast/domain/config/ConfigItems.java
@@ -1,8 +1,11 @@
 package de.jquast.domain.config;
 
 public enum ConfigItems {
-    MODEL_OUTPUT_FORMAT("MODEL_OUTPUT_FORMAT", new String[]{ "csv", "txt" }, "csv"),
-    MODEL_OUTPUT_DIRECTORY("MODEL_OUTPUT_DIRECTORY", null, ".");
+    MODEL_OUTPUT_FORMAT("DEFAULT_MODEL_OUTPUT_FORMAT", new String[]{ "csv", "txt" }, "csv"),
+    MODEL_OUTPUT_DIRECTORY("DEFAULT_MODEL_OUTPUT_DIRECTORY", null, "."),
+    ALGORITHM_TIME_STEPS("DEFAULT_ALGORITHM_TIME_STEPS", null, "100"),
+    ALGORITHM_EXPLORATION_RATE("DEFAULT_ALGORITHM_EXPLORATION_RATE", null, "0.01"),
+    ALGORITHM_DISCOUNT_FACTOR("DEFAULT_ALGORITHM_DISCOUNT", null, "0.05");
 
     private String key;
     private String[] predefinedOptions;
