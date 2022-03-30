9183c2db4d7530aca39c131810f3797f09c3d211
Johannes Quast <johannesquast@outlook.de>
2022-03-30 11:13:29 +0200

    Add simple Data Transfer Objects for Config and RLAlgorithm.

---
+++ 3-ase-reinforcement-learning-adapters/src/main/java/de/jquast/adapters/ConfigItemDTO.java
@@ -0,0 +1,3 @@
+package de.jquast.adapters;
+
+public record ConfigItemDTO(String key, String value) { }
---
+++ 3-ase-reinforcement-learning-adapters/src/main/java/de/jquast/adapters/RLAlgorithmDTO.java
@@ -0,0 +1,3 @@
+package de.jquast.adapters;
+
+public record RLAlgorithmDTO(String name, String description) {}
