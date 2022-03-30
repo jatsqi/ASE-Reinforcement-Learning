c7c6d2b09b985cfba37bcef053a221d81cf3c268
Johannes Quast <johannesquast@outlook.de>
2022-03-30 11:18:54 +0200

    Add more useful toString() method to ConfigItem.

--- 1-ase-reinforcement-learning-domain/src/main/java/de/jquast/domain/config/ConfigItem.java
+++ 1-ase-reinforcement-learning-domain/src/main/java/de/jquast/domain/config/ConfigItem.java
@@ -1,4 +1,9 @@
 package de.jquast.domain.config;
 
 public record ConfigItem(String name, String value) {
+
+    @Override
+    public String toString() {
+        return name + " = '" + value + "'";
+    }
 }
