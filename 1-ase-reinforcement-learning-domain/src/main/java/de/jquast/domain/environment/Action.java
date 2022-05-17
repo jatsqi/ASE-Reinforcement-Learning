daada985ab01efdfc3b32f7f1858175f355117b6
Johannes Quast <johannesquast@outlook.de>
2022-05-17 12:32:17 +0200

    Add enum with possible actions.

---
+++ 1-ase-reinforcement-learning-domain/src/main/java/de/jquast/domain/environment/Action.java
@@ -0,0 +1,29 @@
+package de.jquast.domain.environment;
+
+public enum Action {
+
+    DO_NOTHING(-1, "Faulenzen und nichts tun"),
+    MOVE_X_UP(0, "Bewegung in positive X Richtung"),
+    MOVE_Y_UP(1, "Bewegung in positive Y Richtung"),
+    MOVE_Z_UP(2, "Bewegung in positive Z Richtung"),
+    MOVE_X_DOWN(3, "Bewegung in negative X Richtung"),
+    MOVE_Y_DOWN(4, "Bewegung in positive Y Richtung"),
+    MOVE_Z_DOWN(5, "Bewegung in positive Z Richtung"),
+    PULL(6, "Ziehe am Hebel");
+
+    private int id;
+    private String description;
+
+    Action(int id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
