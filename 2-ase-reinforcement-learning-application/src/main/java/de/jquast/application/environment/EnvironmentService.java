dd1aa9f1e3e920c1a64052266acf1bf2e333affc
Johannes Quast <johannesquast@outlook.de>
2022-05-17 15:48:00 +0200

    Add simple Environment Service.

---
+++ 2-ase-reinforcement-learning-application/src/main/java/de/jquast/application/environment/EnvironmentService.java
@@ -0,0 +1,27 @@
+package de.jquast.application.environment;
+
+import de.jquast.domain.environment.EnvironmentDescriptor;
+import de.jquast.domain.environment.EnvironmentRepository;
+import de.jquast.utils.di.annotations.Inject;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public class EnvironmentService {
+
+    private final EnvironmentRepository environmentRepository;
+
+    @Inject
+    public EnvironmentService(EnvironmentRepository environmentRepository) {
+        this.environmentRepository = environmentRepository;
+    }
+
+    public Collection<EnvironmentDescriptor> getEnvironments() {
+        return environmentRepository.getEnvironments();
+    }
+
+    public Optional<EnvironmentDescriptor> getEnvironment(String name) {
+        return environmentRepository.getEnvironment(name);
+    }
+
+}
