29b83bebe8b4eaca78815f0c0f37e835a9f9ba74
Johannes Quast <johannesquast@outlook.de>
2022-05-17 17:11:46 +0200

    Add method to trigger policy update.

--- 1-ase-reinforcement-learning-domain/src/main/java/de/jquast/domain/shared/ActionSource.java
+++ 1-ase-reinforcement-learning-domain/src/main/java/de/jquast/domain/shared/ActionSource.java
@@ -2,6 +2,13 @@ package de.jquast.domain.shared;
 
 public interface ActionSource {
 
-    Action selectAction(int state);
+    /**
+     * Selektiert eine passende Aktion im State.
+     * @param state     Der State.
+     * @return          Die gewählte Aktion.
+     */
+    int selectAction(int state);
+
+    void criticiseAction(int oldState, int action, int newState, double reward);
 
 }
--- 4-ase-reinforcement-learning-plugin/src/main/java/de/jquast/plugin/algorithm/QLearning.java
+++ 4-ase-reinforcement-learning-plugin/src/main/java/de/jquast/plugin/algorithm/QLearning.java
@@ -1,12 +1,22 @@
 package de.jquast.plugin.algorithm;
 
 import de.jquast.domain.algorithm.RLAlgorithm;
+import de.jquast.domain.policy.Policy;
 import de.jquast.domain.shared.Action;
 
 public class QLearning extends RLAlgorithm {
 
+    public QLearning(Policy basePolicy) {
+        super(basePolicy);
+    }
+
     @Override
-    public Action selectAction(int state) {
-        return null;
+    public int selectAction(int state) {
+        return 0;
+    }
+
+    @Override
+    public void criticiseAction(int oldState, int action, int newState, double reward) {
+
     }
 }
