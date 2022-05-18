154b827496de762c38bd1c29d6b28c89a25f7e46
i517917 <johannes.quast@sap.com>
2022-05-18 15:04:27 +0200

    Add Service which extracts RLSettings from the config repository.

---
+++ 2-ase-reinforcement-learning-application/src/main/java/de/jquast/application/service/RLSettingsService.java
@@ -0,0 +1,32 @@
+package de.jquast.application.service;
+
+import de.jquast.domain.algorithm.RLSettings;
+import de.jquast.domain.config.ConfigItem;
+import de.jquast.domain.config.ConfigRepository;
+import de.jquast.domain.config.DefaultConfigItem;
+
+public class RLSettingsService {
+
+    private ConfigRepository configRepository;
+
+    public RLSettingsService(ConfigRepository configRepository) {
+        this.configRepository = configRepository;
+    }
+
+    public RLSettings getRLSettings() {
+        RLSettings settings = new RLSettings(
+                getDoubleConfigItem(DefaultConfigItem.ALGORITHM_LEARNING_RARE),
+                getDoubleConfigItem(DefaultConfigItem.ALGORITHM_DISCOUNT_FACTOR),
+                getDoubleConfigItem(DefaultConfigItem.ALGORITHM_EXPLORATION_RATE)
+        );
+
+        return settings;
+    }
+
+    private double getDoubleConfigItem(DefaultConfigItem item) {
+        return Double.parseDouble(
+                configRepository.getConfigItem(item.getKey())
+                        .orElse(new ConfigItem("", "0.0"))
+                        .value());
+    }
+}
