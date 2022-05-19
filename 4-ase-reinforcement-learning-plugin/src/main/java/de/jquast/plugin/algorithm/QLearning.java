65d32a613d9d0e7fb9458ff62a8191d3486cbe36
Johannes Quast <johannesquast@outlook.de>
2022-05-19 10:39:45 +0200

    Delegate call to criticizeAction.

--- 4-ase-reinforcement-learning-plugin/src/main/java/de/jquast/plugin/algorithm/QLearning.java
+++ 4-ase-reinforcement-learning-plugin/src/main/java/de/jquast/plugin/algorithm/QLearning.java
@@ -18,6 +18,8 @@ public class QLearning extends RLAlgorithm {
 
     @Override
     public void criticiseAction(int oldState, int action, int newState, double reward) {
+        actionSourceDelegate.criticiseAction(oldState, action, newState, reward);
+
         ActionValueStore.ActionValueEntry maxEntry = actionValueStoreDelegate.getMaxActionValue(newState);
 
         double oldValue = actionValueStoreDelegate.getActionValue(oldState, action);
