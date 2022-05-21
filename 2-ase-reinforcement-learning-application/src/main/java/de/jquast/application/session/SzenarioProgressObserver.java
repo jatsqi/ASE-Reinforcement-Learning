4f3d9ec9650db81a1d22a3e1df0e2685dfedd82b
Johannes Quast <johannesquast@outlook.de>
2022-05-21 14:13:54 +0200

    Renamed file and changed method signatures.

---
+++ 2-ase-reinforcement-learning-application/src/main/java/de/jquast/application/session/SzenarioProgressObserver.java
@@ -0,0 +1,13 @@
+package de.jquast.application.session;
+
+public interface SzenarioProgressObserver {
+
+    default void onTrainingStart(SzenarioSession session) {};
+
+    default void preTrainingStep(SzenarioSession session, long currentStep, double averageReward) {};
+
+    default void postTrainingStep(SzenarioSession session, long currentStep, double averageReward) {};
+
+    default void onTrainingEnd(SzenarioSession session, long currentStep, double averageReward) {};
+
+}
