24c44969aa2153d42fde255d37a7f77005c2da92
Johannes Quast <johannesquast@outlook.de>
2022-05-27 11:46:23 +0200

    Remove accidental code duplication in RLSettings.

--- 1-ase-reinforcement-learning-domain/src/main/java/de/jquast/domain/algorithm/RLSettings.java
+++ 1-ase-reinforcement-learning-domain/src/main/java/de/jquast/domain/algorithm/RLSettings.java
@@ -4,17 +4,10 @@ public record RLSettings(double learningRate, double discountFactor, double expl
                          double agentRewardStepSize) {
 
     public RLSettings {
-        if (learningRate < 0 || learningRate > 1)
-            throw new IllegalArgumentException("Die Lernrate darf nur im Interval [0, 1] liegen.");
-
-        if (discountFactor < 0 || discountFactor > 1)
-            throw new IllegalArgumentException("Der Discount Factor darf nur im Interval [0, 1] liegen.");
-
-        if (explorationRate < 0 || explorationRate > 1)
-            throw new IllegalArgumentException("Die Erkundungsrate darf nur im Interval [0, 1] liegen.");
-
-        if (agentRewardStepSize < 0 || agentRewardStepSize > 1)
-            throw new IllegalArgumentException("Die Agent-Reward-Schrittrate darf nur im Interval [0, 1] liegen.");
+        checkArgumentRangeZeroToOneInclusive(learningRate, "Die Lernrate darf nur im Interval [0, 1] liegen.");
+        checkArgumentRangeZeroToOneInclusive(discountFactor, "Der Discount Factor darf nur im Interval [0, 1] liegen.");
+        checkArgumentRangeZeroToOneInclusive(explorationRate, "Die Erkundungsrate darf nur im Interval [0, 1] liegen.");
+        checkArgumentRangeZeroToOneInclusive(agentRewardStepSize, "Die Agent-Reward-Schrittrate darf nur im Interval [0, 1] liegen.");
     }
 
     @Override
@@ -24,5 +17,10 @@ public record RLSettings(double learningRate, double discountFactor, double expl
                 "\n  Discount Factor: " + discountFactor +
                 "\n  Erkundungsfaktor: " + explorationRate +
                 "\n  Reward Step-Size: " + agentRewardStepSize;
+    }
+
+    private static void checkArgumentRangeZeroToOneInclusive(double value, String error) {
+        if (value < 0 || value > 1)
+            throw new IllegalArgumentException(error);
     }
 }
