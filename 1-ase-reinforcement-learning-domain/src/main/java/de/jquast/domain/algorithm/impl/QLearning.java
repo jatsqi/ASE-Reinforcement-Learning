a99894ac307b544415d2b0098e53c8e80a333efb
Johannes Quast <johannesquast@outlook.de>
2022-05-28 23:38:37 +0200

    Extend documentation.

--- 1-ase-reinforcement-learning-domain/src/main/java/de/jquast/domain/algorithm/impl/QLearning.java
+++ 1-ase-reinforcement-learning-domain/src/main/java/de/jquast/domain/algorithm/impl/QLearning.java
@@ -6,6 +6,11 @@ import de.jquast.domain.algorithm.RLSettings;
 import de.jquast.domain.shared.ActionSource;
 import de.jquast.domain.shared.ActionValueStore;
 
+/**
+ * Grundlegender Algorithmus, der Teil der Domäne ist.
+ * Dies stellt GRUNDLEGENDE Funktionalität dar, egal wie die Applikation konkret aussieht oder welche Use-Cases diese
+ * beinhaltet.
+ */
 public class QLearning extends RLAlgorithm {
 
     public static final RLAlgorithmDescriptor Q_LEARNING_ALGORITHM_DESCRIPTOR = new RLAlgorithmDescriptor(
